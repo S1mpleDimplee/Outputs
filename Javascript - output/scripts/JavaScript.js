adaf55d4129e39f9c4ce1649e5ae9e12de2b957d
S1mpleDimplee <144920946+S1mpleDimplee@users.noreply.github.com>
2024-02-07 22:53:19 +0100

    Add files via upload

---
+++ Javascript - output/scripts/JavaScript.js
@@ -0,0 +1,9 @@
+function ConsoleMessage() {
+    console.log("Hello, World!");
+}
+function AlertMessage() {
+    alert("Hello, World!");
+}
+function ChangeText() {
+    document.getElementById("Text").innerHTML = "Hello, World!";
+}
